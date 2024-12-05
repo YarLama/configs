4eeb2cb48566b82af5670b695c27c6b44cc0b2e0
YarLama <gaideichuk1998@mail.ru>
2024-12-05 10:21:47 +0300

    #0.7. few changes in mason

--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -1,23 +1,25 @@
 require("mason").setup()
 require("mason-lspconfig").setup {
-    ensure_installed = { "lua_ls", "ts_ls", "vimls" }
+    ensure_installed = { "lua_ls", "ts_ls" }
 }
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require("lspconfig")
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
     local opts = {buffer = bufnr, remap = false}
 
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_next(), opts)
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_prev(), opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
 
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references , opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 end
 
 local cmp = require('cmp')
@@ -54,4 +56,3 @@ lspconfig.lua_ls.setup({
        }
    }
 })
-lspconfig.vimls.setup({})
