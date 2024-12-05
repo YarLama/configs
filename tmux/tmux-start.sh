b9b2037dbdc4750a1cd83ad95b3285c09dc0a28f
YarLama <gaideichuk1998@mail.ru>
2024-12-05 12:36:29 +0300

    #0.9. update tmux conf

--- tmux/tmux-start.sh
+++ tmux/tmux-start.sh
@@ -1,34 +1,36 @@
 #!/bin/sh
 
 SESH_NAME="main"
-NVIM_CONFIG_SESH_NAME="nvim_config"
+CONFIGS_SESH="configs"
 
-tmux source ~/.config/tmux/.tmux.conf
 tmux has-session -t $SESH_NAME 2>/dev/null
 
 if [ $? != 0 ]; then
 
 	tmux new-session -d -s $SESH_NAME -n zsh
 
-	tmux new-window -n mc
-	tmux send-keys "sudo mc" Enter
+	tmux new-window -t $SESH_NAME -n mc
+	tmux send-keys -t $SESH_NAME:mc "sudo mc" Enter
 
-	tmux new-window -n htop
-	tmux send-keys "htop" Enter
+	tmux new-window -t $SESH_NAME -n htop
+	tmux send-keys -t $SESH_NAME:htop "htop" Enter
 
 	tmux select-window -t $SESH_NAME:mc
 fi
 
-tmux has-session -t $NVIM_CONFIG_SESH_NAME 2>/dev/null
+tmux has-session -t $CONFIGS_SESH 2>/dev/null
 
 if [ $? != 0 ]; then
 
-	tmux new-session -d -s $NVIM_CONFIG_SESH_NAME -n nvim
+	tmux new-session -d -s $CONFIGS_SESH -n "nvim"
+	tmux send-keys -t $CONFIGS_SESH:nvim "cd ~/.config/nvim/" Enter
+	tmux send-keys -t $CONFIGS_SESH:nvim "nvim ." Enter
 
-	tmux send-keys "cd ~/.config/nvim/" Enter
-	tmux send-keys "nvim ." Enter
+    tmux new-window -t $CONFIGS_SESH -n "tmux"
+	tmux send-keys -t $CONFIGS_SESH:tmux "cd ~/.config/tmux/" Enter
+	tmux send-keys -t $CONFIGS_SESH:tmux "nvim ." Enter
 
-	tmux select-window -t $NVIM_CONFIG_SESH_NAME:nvim
+	tmux select-window -t $CONFIGS_SESH:nvim
 fi
 
-tmux attach-session -t $NVIM_CONFIG_SESH_NAME
+tmux attach-session -t $CONFIGS_SESH
