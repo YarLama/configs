f86156954ae7163239c293048a41b5fd8de799ee
YarLama <gaideichuk1998@mail.ru>
2024-12-22 09:13:04 +0300

    #0.17. add none-ls, install prettier/stylua

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,41 +1,41 @@
-require('lualine').setup {
-  options = {
-    icons_enabled = false,
-    theme = 'nord',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
+require("lualine").setup({
+    options = {
+        icons_enabled = false,
+        theme = "nord",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        always_show_tabline = true,
+        globalstatus = false,
+        refresh = {
+            statusline = 100,
+            tabline = 100,
+            winbar = 100,
+        },
     },
-    ignore_focus = {},
-    always_divide_middle = true,
-    always_show_tabline = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 100,
-      tabline = 100,
-      winbar = 100,
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'location'},
-    lualine_c = {'filename'},
-    lualine_x = {'diagnostics', 'branch', 'encoding', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
-}
+    sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "location" },
+        lualine_c = { "filename" },
+        lualine_x = { "diagnostics", "branch", "encoding", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = {},
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {},
+})
--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,30 +1,30 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Telescope find git files' })
-vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = 'Telescope view git files status' })
-vim.keymap.set('n', '<leader>gl', builtin.git_commits, { desc = 'Telescope view git commits' })
-vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = 'Telescope view git branches' })
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Telescope find git files" })
+vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Telescope view git files status" })
+vim.keymap.set("n", "<leader>gl", builtin.git_commits, { desc = "Telescope view git commits" })
+vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "Telescope view git branches" })
+vim.keymap.set("n", "<leader>gg", function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 
-require('telescope').setup {
+require("telescope").setup({
     defaults = {},
     pickers = {
         find_files = {
-            disable_devicons = true
+            disable_devicons = true,
         },
         git_files = {
-            disable_devicons = true
+            disable_devicons = true,
         },
         git_status = {
-            disable_devicons = true
+            disable_devicons = true,
         },
         git_commits = {
-            disable_devicons = true
+            disable_devicons = true,
         },
         git_branches = {
-            disable_devicons = true
+            disable_devicons = true,
         },
     },
-}
+})
