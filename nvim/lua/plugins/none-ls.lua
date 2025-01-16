6e55d63f49b20ae810282aeacf23dbfad2ee52cb
YarLama <gaideichuk1998@mail.ru>
2025-01-16 11:14:09 +0300

    #0.21. add exec condtion for eslint_d linter

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,19 +1,23 @@
 return {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-        "nvimtools/none-ls-extras.nvim",
-    },
-    config = function()
-        local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
 
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.prettier,
-                null_ls.builtins.formatting.stylua,
-                require("none-ls.diagnostics.eslint_d"),
-            },
-        })
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.stylua,
+				require("none-ls.diagnostics.eslint_d").with({
+					condition = function(utils)
+						return utils.root_has_file({ ".eslintrc", ".eslintrc.json", ".eslintrc.js", "eslint.config.js" })
+					end,
+				}),
+			},
+		})
 
-        vim.keymap.set("n", "<leader>pf", vim.lsp.buf.format, {})
-    end,
+		vim.keymap.set("n", "<leader>pf", vim.lsp.buf.format, {})
+	end,
 }
