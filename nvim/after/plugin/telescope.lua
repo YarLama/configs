fc4a4fe4b1846dc0e6808a94c124b2a9a2856c03
YarLama <gaideichuk1998@mail.ru>
2025-02-14 13:37:45 +0300

    #0.25. add Telescope undotree, and change keymaps

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,32 +1,52 @@
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Telescope find git files" })
-vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Telescope view git files status" })
-vim.keymap.set("n", "<leader>gl", builtin.git_commits, { desc = "Telescope view git commits" })
-vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "Telescope view git branches" })
-vim.keymap.set("n", "<leader>gg", function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Telescope find git files" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope find help info" })
+vim.keymap.set("n", "<leader>fs", builtin.git_status, { desc = "Telescope view git files status" })
+vim.keymap.set("n", "<leader>fc", builtin.git_commits, { desc = "Telescope view git commits" })
+vim.keymap.set("n", "<leader>fb", builtin.git_branches, { desc = "Telescope view git branches" })
+vim.keymap.set("n", "<leader>fu", "<cmd>Telescope undo<cr>", { desc = "Telescope undo extenstion" })
+vim.keymap.set("n", "<leader>fg", function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 
 require("telescope").setup({
-    defaults = {
-        file_ignore_patterns = {"node_modules"}
-    },
-    pickers = {
-        find_files = {
-            disable_devicons = true,
-        },
-        git_files = {
-            disable_devicons = true,
-        },
-        git_status = {
-            disable_devicons = true,
-        },
-        git_commits = {
-            disable_devicons = true,
-        },
-        git_branches = {
-            disable_devicons = true,
-        },
-    },
+	defaults = {
+		file_ignore_patterns = { "node_modules" },
+	},
+	extenstion = {
+		undo = {
+			mappings = {
+				i = {
+					["<cr>"] = require("telescope-undo.actions").yank_additions,
+					["<C-y>"] = require("telescope-undo.actions").yank_deletions,
+					["<C-r>"] = require("telescope-undo.actions").restore,
+				},
+				n = {
+					["y"] = require("telescope-undo.actions").yank_additions,
+					["Y"] = require("telescope-undo.actions").yank_deletions,
+					["u"] = require("telescope-undo.actions").restore,
+				},
+			},
+		},
+	},
+	pickers = {
+		find_files = {
+			disable_devicons = true,
+		},
+		git_files = {
+			disable_devicons = true,
+		},
+		git_status = {
+			disable_devicons = true,
+		},
+		git_commits = {
+			disable_devicons = true,
+		},
+		git_branches = {
+			disable_devicons = true,
+		},
+	},
 })
+
+require("telescope").load_extension("undo")
