051179adc9146abc39907d0779d7e9b9f3983bb8
YarLama <gaideichuk1998@mail.ru>
2025-02-14 15:52:25 +0300

    #0.27. change mapping function

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,3 +1,22 @@
 local map = vim.keymap.set
-map("n", "<leader>gg", ":Git | wincmd L | :Git log --oneline | wincmd j<CR>", { noremap = true, silent = true })
-map("n", "<leader>gc", ":wincmd h | wincmd o<CR>", { noremap = true, silent = true })
+local startWin = nil
+
+map("n", "<leader>gg", function()
+	if not startWin then
+		startWin = vim.api.nvim_get_current_win()
+		vim.cmd("Git")
+		vim.cmd("wincmd L")
+		vim.cmd("Git log --oneline")
+		vim.cmd("wincmd j")
+	end
+end, { noremap = true, silent = true })
+
+map("n", "<leader>gc", function()
+	if startWin then
+		vim.api.nvim_set_current_win(startWin)
+		vim.cmd("wincmd o")
+	else
+		print("Press <leader>gg for open Git")
+	end
+  startWin = nil
+end, { noremap = true, silent = true })
