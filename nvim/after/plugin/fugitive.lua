c61c941f5b5be605bb1dfeb11da6cdc94e186559
YarLama <gaideichuk1998@mail.ru>
2025-02-16 10:05:09 +0300

    #0.29. some changes

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,22 +1,25 @@
 local map = vim.keymap.set
 local startWin = nil
+local openKey = "<leader>gg"
+local closeKey = "<leader>gc"
 
-map("n", "<leader>gg", function()
+map("n", openKey, function()
 	if not startWin then
 		startWin = vim.api.nvim_get_current_win()
 		vim.cmd("Git")
 		vim.cmd("wincmd L")
 		vim.cmd("Git log --oneline")
 		vim.cmd("wincmd j")
+    print("Press " .. closeKey .. " to close Git window")
 	end
 end, { noremap = true, silent = true })
 
-map("n", "<leader>gc", function()
+map("n", closeKey, function()
 	if startWin then
 		vim.api.nvim_set_current_win(startWin)
 		vim.cmd("wincmd o")
 	else
-		print("Press <leader>gg for open Git")
+		print("Press " .. openKey .. " for open Git window")
 	end
   startWin = nil
 end, { noremap = true, silent = true })
