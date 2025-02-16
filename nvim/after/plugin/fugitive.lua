cd1a4961c7e358324f9d64bc068a04ac50a9457a
YarLama <gaideichuk1998@mail.ru>
2025-02-16 10:40:22 +0300

    #0.30. add commit function

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,7 +1,15 @@
 local map = vim.keymap.set
 local startWin = nil
 local openKey = "<leader>gg"
-local closeKey = "<leader>gc"
+local closeKey = "<leader>gq"
+local commitKey = "<leader>gc"
+local function getUserInput(prompt)
+	local ok, result = pcall(vim.fn.input, prompt)
+	if not ok then
+		return nil
+	end
+	return result
+end
 
 map("n", openKey, function()
 	if not startWin then
@@ -10,7 +18,7 @@ map("n", openKey, function()
 		vim.cmd("wincmd L")
 		vim.cmd("Git log --oneline")
 		vim.cmd("wincmd j")
-    print("Press " .. closeKey .. " to close Git window")
+		print("Press " .. closeKey .. " to close Git window")
 	end
 end, { noremap = true, silent = true })
 
@@ -21,5 +29,12 @@ map("n", closeKey, function()
 	else
 		print("Press " .. openKey .. " for open Git window")
 	end
-  startWin = nil
+	startWin = nil
 end, { noremap = true, silent = true })
+
+map("n", commitKey, function()
+	local commitMessage = getUserInput("Commit message: ")
+	if commitMessage and commitMessage ~= "" then
+    vim.cmd("Git commit -m '" .. commitMessage .. "'")
+	end
+end)
