03ae2292aa50a7f037934c6e22884447b8ec6580
YarLama <gaideichuk1998@mail.ru>
2025-02-19 16:33:24 +0300

    #0.35. updates colors

--- dwm/config.h
+++ dwm/config.h
@@ -5,19 +5,19 @@ static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;	/* snap pixel */
 static const int showbar = 1;			/* 0 means no bar */
 static const int topbar = 0;			/* 0 means bottom bar */
-// static const char *fonts[] = {"Open Sans:size=10:style=Regular"};
-// static const char dmenufont[] = "Open Sans:size=10:style=Regular";
 static const char *fonts[] = {"Noto Sans:size=11"};
 static const char dmenufont[] = "Noto Sans:size=11";
+static const char col_black1[] = "#000000";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
+static const char col_gray5[] = "#262c36";
 static const char col_cyan[] = "#005577";
 static const char *colors[][3] = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = {col_gray3, col_gray1, col_gray2},
-	[SchemeSel] = {col_gray4, col_cyan, col_cyan},
+	[SchemeNorm] = {col_gray4, col_black1, col_black1},
+	[SchemeSel] = {col_gray4, col_gray5, col_black1},
 };
 
 static const char *const autostart[] = {
