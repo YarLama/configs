ebb4c63aecba293a2990ef4dc2c432e49311a3ea
YarLama <gaideichuk1998@mail.ru>
2025-02-19 06:52:39 +0300

    #0.33. some keymaps

--- nvim/lua/yarlama/remap.lua
+++ nvim/lua/yarlama/remap.lua
@@ -2,6 +2,7 @@ local map = vim.keymap.set
 
 vim.g.mapleader = " "
 map("n", "<leader>e", vim.cmd.Ex)
+map("n", "L", vim.cmd.Lazy)
 
 map("v", "<C-c>", '"+y')
 map("n", "<C-p>", '"+p')
@@ -14,4 +15,5 @@ map("n", "<leader>o", 'o<Esc>')
 
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
+
 
