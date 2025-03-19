88789e0722711c8dc723d07fac33dfb8adf65160
YarLama <gaideichuk1998@mail.ru>
2025-03-20 01:46:38 +0300

    #0.37. add yamlls for github workflow

--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "ts_ls", "eslint" },
+	ensure_installed = { "lua_ls", "ts_ls", "eslint", "yamlls" },
 })
 
 local map = vim.keymap.set
@@ -50,6 +50,36 @@ local on_attach = function(client, bufnr)
 	client.server_capabilities.documentRangeFormattingProvider = false
 end
 
+local on_attach_yaml = function(client, bufnr)
+	local opts = { buffer = bufnr, remap = false }
+
+	map("n", "<leader>lr", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	map("n", "<leader>ls", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
+	map("n", "[d", function()
+		vim.diagnostic.goto_next()
+	end, opts)
+	map("n", "]d", function()
+		vim.diagnostic.goto_prev()
+	end, opts)
+	map("n", "<leader>lf", function()
+		vim.diagnostic.open_float()
+	end, opts)
+
+	map("n", "gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	map("n", "gi", function()
+		vim.lsp.buf.implementation()
+	end, opts)
+	map("n", "K", function()
+		vim.lsp.buf.hover()
+	end, opts)
+end
+
 local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
@@ -91,4 +121,19 @@ lspconfig.lua_ls.setup({
 			},
 		},
 	},
+})
+lspconfig.yamlls.setup({
+	capabilities = capabilities,
+	settings = {
+		yaml = {
+			schemas = {
+				["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*.yml",
+			},
+			format = { enabled = true },
+			validate = true,
+			hover = true,
+			completion = true,
+		},
+	},
+	on_attach = on_attach_yaml,
 })
