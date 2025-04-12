faa116b1c49c28d29156f7d00e4005a6a01c478a
YarLama <gaideichuk1998@mail.ru>
2025-04-12 20:20:29 +0300

    #0.43. added label for Prettier

--- nvim/after/plugin/none-ls.lua
+++ nvim/after/plugin/none-ls.lua
@@ -3,7 +3,12 @@ local map = vim.keymap.set
 
 null_ls.setup({
 	sources = {
-		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.prettier.with({
+      runtime_condition = function (params)
+        print("[Prettier]: Format file: " .. params.bufname)
+        return true
+      end
+    }),
 		null_ls.builtins.formatting.stylua,
 		require("none-ls.diagnostics.eslint_d").with({
 			condition = function(utils)
