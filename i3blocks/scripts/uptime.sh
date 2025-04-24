6642982426b9fdf1cfc06a0383f84ea9bb3fdf56
YarLama <yarlamas@gmail.com>
2025-04-24 03:21:56 +0300

    #0.54. correct uptime.sh

--- i3blocks/scripts/uptime.sh
+++ i3blocks/scripts/uptime.sh
@@ -3,7 +3,7 @@
 check_min=$(uptime | awk '{print $4}')
 uptime=$(uptime | awk '{print $3}')
 if [[ "${check_min}" == "min," ]]; then
-  if [ ${uptime} -lt 10]; then
+  if [[ ${uptime} -lt 10 ]]; then
     echo "0:0${uptime}"
   else
     echo "0:${uptime}"
