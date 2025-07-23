f23a258aa81ab930b3afa1b132fe1cd4a73746f1
YarLama <yarlamas@gmail.com>
2025-07-23 03:44:01 +0300

    #0.59. config nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -3,7 +3,9 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("nvim-tree").setup({
-
+      git = {
+        enable = false,
+      }
     })
   end,
 }
