3698ed965d106997278fecff9a83e2420c7b719f
YarLama <yarlamas@gmail.com>
2025-10-15 11:40:54 +0300

    #0.67. used vim.lsp.config instead of require("lspconfig")

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,26 +1,25 @@
-local lspconfig = require("lspconfig")
 local shared = dofile(vim.fn.stdpath("config") .. "/after/plugin/lsp/init.lua")
-
 local servers = {
-	"lua_ls",
-	"ts_ls",
-	"eslint",
-	"yamlls",
-	"cssls",
-	"tailwindcss",
+  "lua_ls",
+  "ts_ls",
+  "eslint",
+  "yamlls",
+  "cssls",
+  "tailwindcss",
+}
+local default_config = {
+  on_attach = shared.on_attach,
+  capabilities = shared.capabilities,
 }
 
 for _, server in ipairs(servers) do
-	local ok, config = pcall(require, "lsp.servers." .. server)
-	if ok then
-		lspconfig[server].setup(vim.tbl_deep_extend("force", {
-			on_attach = shared.on_attach,
-			capabilities = shared.capabilities,
-		}, config or {}))
-	else
-		lspconfig[server].setup({
-			on_attach = shared.on_attach,
-			capabilities = shared.capabilities,
-		})
-	end
+  local ok, config = pcall(require, "lsp.servers." .. server)
+  if (ok and type(config) == "table") then
+    vim.lsp.config(server, vim.tbl_deep_extend("force", {
+      on_attach = shared.on_attach,
+      capabilities = shared.capabilities,
+    }, config or {}))
+  else
+    vim.lsp.config(server, default_config)
+  end
 end
