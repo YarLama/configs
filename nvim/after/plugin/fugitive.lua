ec7b32d19ce9e16e5cc8834b48f5b2d518375c57
YarLama <yarlamas@gmail.com>
2026-03-31 16:16:23 +0300

    feat: mapping open diff for file

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -3,38 +3,43 @@ local startWin = nil
 local openKey = "<leader>gg"
 local closeKey = "<leader>gq"
 local commitKey = "<leader>gc"
+local diffHeadKey = "<leader>gh"
+local diffMasterKey = "<leader>gm"
 local function getUserInput(prompt)
-	local ok, result = pcall(vim.fn.input, prompt)
-	if not ok then
-		return nil
-	end
-	return result
+  local ok, result = pcall(vim.fn.input, prompt)
+  if not ok then
+    return nil
+  end
+  return result
 end
 
+map("n", diffHeadKey, ":Gvdiffsplit<CR>", { desc = "Show diff current file with HEAD" })
+map("n", diffMasterKey, ":Gvdiffsplit master<CR>", { desc = "Show diff current file with master" })
+
 map("n", openKey, function()
-	if not startWin then
-		startWin = vim.api.nvim_get_current_win()
-		vim.cmd("Git")
-		vim.cmd("wincmd L")
-		vim.cmd("Git log --oneline")
-		vim.cmd("wincmd j")
-		print("Press " .. closeKey .. " to close Git window")
-	end
+  if not startWin then
+    startWin = vim.api.nvim_get_current_win()
+    vim.cmd("Git")
+    vim.cmd("wincmd L")
+    vim.cmd("Git log --oneline")
+    vim.cmd("wincmd j")
+    print("Press " .. closeKey .. " to close Git window")
+  end
 end, { noremap = true, silent = true })
 
 map("n", closeKey, function()
-	if startWin then
-		vim.api.nvim_set_current_win(startWin)
-		vim.cmd("wincmd o")
-	else
-		print("Press " .. openKey .. " for open Git window")
-	end
-	startWin = nil
+  if startWin then
+    vim.api.nvim_set_current_win(startWin)
+    vim.cmd("wincmd o")
+  else
+    print("Press " .. openKey .. " for open Git window")
+  end
+  startWin = nil
 end, { noremap = true, silent = true })
 
 map("n", commitKey, function()
-	local commitMessage = getUserInput("Commit message: ")
-	if commitMessage and commitMessage ~= "" then
+  local commitMessage = getUserInput("Commit message: ")
+  if commitMessage and commitMessage ~= "" then
     vim.cmd("Git commit -m '" .. commitMessage .. "'")
-	end
+  end
 end)
