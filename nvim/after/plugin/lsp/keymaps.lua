2fea99138af2e23eaada54a9aba5ec81eaeaf369
YarLama <yarlamas@gmail.com>
2026-04-17 10:47:20 +0300

    fix: warning about position_encoding (lsp)

--- nvim/after/plugin/lsp/keymaps.lua
+++ nvim/after/plugin/lsp/keymaps.lua
@@ -1,40 +1,47 @@
 local vim = vim
 local map = vim.keymap.set
+local util = require("vim.lsp.util")
 
+--=====================set utf-16 as default for lsp=================
+local old_make_params = util.make_position_params
+util.make_position_params = function(winid, encoding)
+  return old_make_params(winid, encoding or "utf-16")
+end
 
+--===================================================================
 local M = {}
 
 function M.setup(bufnr)
-	local opts = { buffer = bufnr, remap = false }
+  local opts = { buffer = bufnr, remap = false }
 
-	map("n", "<leader>lr", function()
-		vim.lsp.buf.rename()
-	end, opts)
-	map("n", "<leader>la", function()
-		vim.lsp.buf.code_action()
-	end, opts)
-	map("n", "<leader>ls", function()
-		vim.lsp.buf.signature_help()
-	end, opts)
-	map("n", "[d", function()
-		vim.diagnostic.goto_next()
-	end, opts)
-	map("n", "]d", function()
-		vim.diagnostic.goto_prev()
-	end, opts)
-	map("n", "<leader>lf", function()
-		vim.diagnostic.open_float()
-	end, opts)
+  map("n", "<leader>lr", function()
+    vim.lsp.buf.rename()
+  end, opts)
+  map("n", "<leader>la", function()
+    vim.lsp.buf.code_action()
+  end, opts)
+  map("n", "<leader>ls", function()
+    vim.lsp.buf.signature_help()
+  end, opts)
+  map("n", "[d", function()
+    vim.diagnostic.goto_next()
+  end, opts)
+  map("n", "]d", function()
+    vim.diagnostic.goto_prev()
+  end, opts)
+  map("n", "<leader>lf", function()
+    vim.diagnostic.open_float()
+  end, opts)
 
-	map("n", "gd", function()
-		vim.lsp.buf.definition()
-	end, opts)
-	map("n", "gi", function()
-		vim.lsp.buf.implementation()
-	end, opts)
-	map("n", "K", function()
-		vim.lsp.buf.hover()
-	end, opts)
+  map("n", "gd", function()
+    vim.lsp.buf.definition()
+  end, opts)
+  map("n", "gi", function()
+    vim.lsp.buf.implementation()
+  end, opts)
+  map("n", "K", function()
+    vim.lsp.buf.hover()
+  end, opts)
 end
 
 return M
